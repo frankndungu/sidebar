e7e6759cf9d1be461a5c0dd10f95c39f618f5452
frank ndungu <ndungufrank01@gmail.com>
2021-11-10 22:47:34 +0300

    initilization

---
+++ main.js
@@ -0,0 +1,29 @@
+//  navbar toggle
+
+const showMenu = (headerToggle, navbarId) => {
+  const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId);
+
+  // validate that variables exist
+  if (headerToggle && navbarId) {
+    toggleBtn.addEventListener("click", () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle("show-menu");
+      // change icon
+      toggleBtn.classList.toggle("bx-x");
+    });
+  }
+};
+
+showMenu("header-toggle", "navbar");
+
+//link active
+
+const linkColor = document.querySelectorAll(".nav__link");
+
+function colorLink() {
+  linkColor.forEach((l) => l.classList.remove("active"));
+  this.classList.add("active");
+}
+
+linkColor.forEach((l) => l.addEventListener("click", colorLink));
